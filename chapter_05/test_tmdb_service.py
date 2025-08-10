30739f494086cb2a64f0022a45db761954f9b3a0
haytran <hay.tran@employmenthero.com>
2025-08-10 23:47:32 +0700

    update test tmdb service

--- chapter_05/test_tmdb_service.py
+++ chapter_05/test_tmdb_service.py
@@ -7,32 +7,32 @@ async def main():
     # Initialize the kernel
     kernel = sk.Kernel()
 
-    tmdb_service = kernel.import_plugin_from_object(TMDbService(), "TMDBService")
+    tmdb_service = kernel.add_plugin(TMDbService(), "TMDBService")
 
     # Test the function with the inputs.
     # uncomment the line to test the function
     print(
         await tmdb_service["get_movie_genre_id"](
-            kernel, sk.KernelArguments(genre_name="action")
+            kernel, sk.functions.KernelArguments(genre_name="action")
         )
     )
     print(
         await tmdb_service["get_tv_show_genre_id"](
-            kernel, sk.KernelArguments(genre_name="action")
+            kernel, sk.functions.KernelArguments(genre_name="action")
         )
     )
     print(
         await tmdb_service["get_top_movies_by_genre"](
-            kernel, sk.KernelArguments(genre_name="action")
+            kernel, sk.functions.KernelArguments(genre_name="action")
         )
     )
     print(
         await tmdb_service["get_top_tv_shows_by_genre"](
-            kernel, sk.KernelArguments(genre_name="action")
+            kernel, sk.functions.KernelArguments(genre_name="action")
         )
     )
-    print(await tmdb_service["get_movie_genres"](kernel, sk.KernelArguments()))
-    print(await tmdb_service["get_tv_show_genres"](kernel, sk.KernelArguments()))
+    print(await tmdb_service["get_movie_genres"](kernel, sk.functions.KernelArguments()))
+    print(await tmdb_service["get_tv_show_genres"](kernel, sk.functions.KernelArguments()))
 
 
 # Run the main function
